f242afaa78965de4c54d480765cefaf2134f8e03
manrason <manrason2@gmail.com>
2024-01-10 20:46:53 +0100

    initial commit

---
+++ src/test/java/com/manrason/financialtracker/FinancialTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manrason.financialtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
