2787acabc51fc47404068d31d3c07e3e7c5687a2
manrason <manrason2@gmail.com>
2024-01-10 21:03:41 +0100

    add expense repository findExpenseByName

--- src/main/java/com/manrason/financialtracker/repository/ExpenseRepository.java
+++ src/main/java/com/manrason/financialtracker/repository/ExpenseRepository.java
@@ -4,6 +4,10 @@ import com.manrason.financialtracker.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    Optional<Expense> findExpenseByName(String expenseName);
 }
